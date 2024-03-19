fc39a422139f373d70ac50d1e8e51dfca87e18f1
salihabrik <briksaliha@gmail.com>
2024-03-19 13:41:50 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import backgroundImage from './src/components/background.jpg';
 import "./App.css";
 import Button from "@mui/material/Button";
 import { Container } from "@mui/material";
 import MainCont from "./components/MainCont";
 
 function App() {
+  const style = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    display: "flex",
+    justifyContent: "center",
+    width: "100vw",
+  };
+
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          width: "100vw",
-        }}
-      >
-        <Container maxWidth="xl">
-          <MainCont />
-        </Container>
-      </div>
-    </>
+    <div style={style}>
+      <Container maxWidth="xl">
+        <MainCont />
+      </Container>
+    </div>
   );
 }
 
